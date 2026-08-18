251a75080c55c5b3ff69f7d56abe14a75ff926d8
CHINMAYA KUMAR DAS <chinmayakumardas2000@gmail.com>
2026-08-18 22:13:31 +0530

    day 3

---
+++ Phase 1 — Foundations/app.js
@@ -0,0 +1,27 @@
+// console.log("Hello");
+
+
+// console.warn("this si warnign");
+// console.error("error")
+// const users = [
+//   { name: "John", age: 25 },
+//   { name: "Sarah", age: 28 },
+//   { name: "Mike", age: 30 }
+// ];
+
+// console.table(users);
+
+
+// const c
+// console.log(c);
+console.log(Number("hello"));
+
+
+let x = 5;
+console.log(x++);
+console.log(0 === false);
+
+
+const name = prompt("What is your name?");
+
+console.log(name);
---
+++ Phase 1 — Foundations/datatypes.js
@@ -0,0 +1,8 @@
+console.log(typeof "Hello");
+console.log(typeof 25);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof function() {});
